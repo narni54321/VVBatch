794105499f16ad24cab96f225fb2e1c767563650
narni54321 <122954876+narni54321@users.noreply.github.com>
2023-03-26 09:58:40 +0530

    Update Simple - Copy.java
    Modified the comments, its for coding standards

--- Simple - Copy.java
+++ Simple - Copy.java
@@ -1,3 +1,8 @@
+#Developer : Narni
+#Purpose of this program to print Hello World
+#Date: March 26th 2023
+
+
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello Java");  
